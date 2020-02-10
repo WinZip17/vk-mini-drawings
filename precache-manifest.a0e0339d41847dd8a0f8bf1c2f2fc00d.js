cb696279c96c5b65919700c8e1ea45b744c98da4
WinZip <wydo@protonmail.com>
2020-02-10 21:17:30 +0300

    Updates

--- precache-manifest.a0e0339d41847dd8a0f8bf1c2f2fc00d.js
+++ precache-manifest.a0e0339d41847dd8a0f8bf1c2f2fc00d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a0204dc552d8e58ea97e960bd64db21e",
+    "revision": "c7d72c93540717763e72390e998f2809",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.4e89ce74.chunk.css"
   },
   {
-    "revision": "392d6bee9f58dd3a279a",
+    "revision": "8fdfe493b8f62cb84fc4",
     "url": "./static/css/main.ec0ea42d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.d35dbfa2.chunk.js"
   },
   {
-    "revision": "392d6bee9f58dd3a279a",
-    "url": "./static/js/main.60db6b8a.chunk.js"
+    "revision": "8fdfe493b8f62cb84fc4",
+    "url": "./static/js/main.31969b19.chunk.js"
   },
   {
     "revision": "7baf8ab8c4c1543f5d99",
